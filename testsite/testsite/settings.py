c9c7fa0ec5d8da599216a121d972d72a3f71d6e7
Melissa Garza <melissa@zeitcode.com>
2013-05-09 15:34:13 -0500

    Set up Django testing project and app

---
+++ testsite/testsite/settings.py
@@ -0,0 +1,103 @@
+# Django settings for testsite project.
+
+import os
+DIRNAME = os.sep.join(os.path.abspath(__file__).split(os.sep)[:-2])
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Testing Testerson', 'test@example.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'django-accounts.db',
+    }
+}
+
+TIME_ZONE = 'America/Chicago'
+LANGUAGE_CODE = 'en-us'
+
+SITE_ID = 1
+
+USE_I18N = False
+USE_L10N = True
+USE_TZ = True
+
+MEDIA_ROOT = os.path.join(DIRNAME, 'media/')
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(DIRNAME, 'files/')
+STATIC_URL = '/files/'
+
+STATICFILES_DIRS = (
+    os.path.join(DIRNAME, 'static/'),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+
+SECRET_KEY = 'dkr+86m#8)$5b*y(c3n)jp5-kwr&amp;y+4a@tvi@rt2^h0e3%mnp^'
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+ROOT_URLCONF = 'testsite.urls'
+
+WSGI_APPLICATION = 'testsite.wsgi.application'
+
+TEMPLATE_DIRS = (
+    os.path.join(DIRNAME, 'templates/'),
+)
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'testapp',
+    # 'viewclass_mixins',
+)
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'handlers': {
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        }
+    },
+    'loggers': {
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    }
+}
---
+++ testsite/testsite/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+from .views import HomePageView
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', HomePageView.as_view(), name='home'),
+    # url(r'^testsite/', include('testsite.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
